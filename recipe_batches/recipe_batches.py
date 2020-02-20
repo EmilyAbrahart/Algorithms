e18fca5546de96f64c88069de2bff5ecb7018de4
EmilyAbrahart <emilyabrahart@gmail.com>
2020-02-20 01:36:56 +0000

    stock prices, recipes batches, eating cookies completed

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,28 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    # Initialise max_batches using the first values in both dictionaries.
+    recipe_values = list(recipe.values())
+    ingredients_values = list(ingredients.values())
+    max_batches = ingredients_values[0] // recipe_values[0]
+
+    for k, v in recipe.items():
+        # Check that the key from recipe is present in ingredients, if not set max_batches to zero.
+        if k in ingredients.keys():
+          # Use floor division to determine how many batches worth of each ingredient are present in ingredients. If the number is lower than the current max_batches, set the max_batches to the number.
+            if ingredients[k] // v < max_batches:
+                max_batches = ingredients[k] // v
+        else:
+            max_batches = 0
+    return max_batches
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,34 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    # Initialise variables to track the current minimum price, the index of the current minimum to ensure it's lower than the potential maximum, and the profit.
+    min_price = prices[0]
+    min_price_index = 0
+    # Initialised as the first potential profit.
+    profit = prices[1] - prices[0]
+
+    # Loop through prices, checking against the potential profit and minimum price.
+    for p in prices:
+        # Check if the current price is lower than the minimum price - if so, set to minimum.
+        if p < min_price:
+            min_price = p
+            min_price_index = prices.index(p)
+        # Check if the profit with the current value is larger than the existing profit, and whether it's index is larger the current minimum index.
+        if p - min_price > profit and prices.index(p) > min_price_index:
+            profit = p - min_price
+
+    return profit
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
